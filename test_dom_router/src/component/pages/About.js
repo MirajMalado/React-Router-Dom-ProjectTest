fb9f6931a5408ae32eea2bd3dbf90d04d274c1f3
MirajMalado <diallomalado8@gmail.com>
2022-02-28 13:00:27 +0000

    Home page

---
+++ test_dom_router/src/component/pages/About.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const About = () =>{
+    return(
+<div className="container">
+    <h1> About pages</h1>
+</div>
+    );
+}
+
+export default About;
---
+++ test_dom_router/src/component/pages/Home.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Home = () =>{
+    return(
+<div className="container">
+    <h1>Hello World</h1>
+</div>
+    );
+}
+
+export default Home;
